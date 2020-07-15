3470e43ca78ef7177ec136f98378a696518b35c4
Mlodovico <murilo.lodovico@outlook.com>
2020-07-15 17:49:35 -0300

    2.13.20 testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,17 @@
-test('plus two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/fakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('Create Appointment', async () => {
+  const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+  const createAppoitment = new CreateAppointmentService(
+    fakeAppointmentsRepository
+  );
+
+  const appointment = await createAppoitment.execute({
+    date: new Date(),
+    provider_id: '2354346456'
+  });
+
+  expect(appointment).toHaveProperty('id');
+  expect(appointment.provider_id).toBe('2354346456')
 });
