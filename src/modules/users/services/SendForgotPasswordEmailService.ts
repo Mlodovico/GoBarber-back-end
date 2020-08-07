f0eeef85df977236a618580304c6a4ac87415aaa
Mlodovico <murilo.lodovico@outlook.com>
2020-08-07 19:31:47 -0300

    2.13.21 recover password test and system

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,6 +5,8 @@ import User from '../infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 
 import IUsersRepository from '../repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequestDTO {
   email: string;
@@ -15,9 +17,25 @@ class SendForgotPasswordEmailService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokenRepository: IUserTokensRepository
   ) {}
 
-  public async execute(data: IRequestDTO): Promise<void> {
+  public async execute({ email }: IRequestDTO): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+
+    if(!user) {
+      throw new AppError('User does not exist');
+    }
+
+    await this.userTokenRepository.generate(user?.id)
+
+    this.mailProvider.sendMail(email, 'Recuperação de senha recebido')
   }
 }
 
