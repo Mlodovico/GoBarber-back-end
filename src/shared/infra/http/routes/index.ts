e1edbcb3d2263b76e6eb5f2a680805536e50986b
Mlodovico <murilo.lodovico@outlook.com>
2020-08-13 15:30:07 -0300

    2.13.25 implemmenting password reset in routes and dependencies injection

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import appointmentRouter from '@modules/appointments/infra/http/routes/appointment.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionRouter from '@modules/users/infra/http/routes/session.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRouter);
 
 
 export default routes;
