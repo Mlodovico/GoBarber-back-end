21dbf90bb972da9afb173a34e42dd0ed3ffc2358
Mlodovico <murilo.lodovico@outlook.com>
2020-09-14 19:48:18 -0300

    1.12.29 including user_id column on appointment creatio

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -19,10 +19,28 @@ class CreateAppointmentService {
     private appointmentsRepository: IAppointmentRepository,
   ) {}
 
-  public async execute({ provider_id, user_id, date}: IRequestDTO): Promise<Appointment> {
+  public async execute({
+    provider_id,
+    user_id,
+    date
+  }: IRequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDay = await this.appointmentsRepository.findByDate(appointmentDate);
+    if(isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment in a past date ");
+    }
+
+    if(user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself");
+    }
+
+    if(getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError("You can only create an appointmente between 8AM and 17PM");
+    }
+
+    const findAppointmentInSameDay = await this.appointmentsRepository.findByDate(
+      appointmentDate
+    );
 
     if(findAppointmentInSameDay) {
       throw new AppError('This appointment is already booked', 401);
