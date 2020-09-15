520fcf4c6ed507f5eae18d2bc6c865819993f676
Mlodovico <murilo.lodovico@outlook.com>
2020-09-15 13:13:32 -0300

    1.12.31 configuring mongodb and coverage tests

--- src/modules/appointments/infra/http/routes/appointment.routes.ts
+++ src/modules/appointments/infra/http/routes/appointment.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
 import AppointmentController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentRouter = Router();
 const appointmentsController = new AppointmentController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentRouter.use(ensureAuthenticated);
 
 appointmentRouter.post('/', appointmentsController.create);
+appointmentRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentRouter;
--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,22 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
+
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
 
-const appointmentRouter = Router();
+const providerRoutes = Router();
+
 const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
-appointmentRouter.use(ensureAuthenticated);
+providerRoutes.use(ensureAuthenticated);
 
-appointmentRouter.get('/', providersController.index);
+providerRoutes.get('/', providersController.index);
+providerRoutes.get('/:id/month-availability', providerMonthAvailabilityController.index);
+providerRoutes.get('/:id/day-availability', providerDayAvailabilityController.index);
 
-export default appointmentRouter;
+
+export default providerRoutes;
